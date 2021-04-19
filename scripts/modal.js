733055b297b3517a565fca72ae0fdb665fbb1e2d
Andrey Fanin <andr.fanin16@yandex.ru>
2021-04-19 14:53:01 +0500

    Правки после код-ревью #3 неделя

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,32 +1,17 @@
-const hamburger = document.querySelector('.hamburger');
-const body = document.body;
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const closeElement = document.querySelector('.hamburger-menu__close');
+let burger  = document.querySelector('.hamburger');
+let overlay = document.querySelector('.hamburger-menu');
+let closer = document.querySelector('.hamburger-menu__closer');
 
-hamburger.addEventListener('click', e => {
-    hamburgerMenu.style.display = 'block';
-});
+let links = document.querySelectorAll('.hamburger-menu__link');
 
-closeElement.addEventListener('click', e => {
-    hamburgerMenu.style.display = 'none';
-});
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
 
-// function createModal(content) {
-//  const overlayElement = document.createElement('div');
-//  overlayElement.classList.add('overlay');
+function toggleMenu(){
+  burger.classList.toggle('hamburger--active');
+  overlay.classList.toggle('hamburger-menu--active');
+  closer.classList.toggle('closer');
+}
 
-//  const template = document.querySelector('#overlayTemplate');
-
-//  overlayElement.innerHTML = template.innerHTML;
-//  overlayElement.addEventListener('click', e => {
-//    if (e.target === overlayElement) {
-//      closeElement.click();
-//    }
-//  })
-
-
-//  const contentElement = overlayElement.querySelector('.content');
-//  contentElement.innerHTML = content;
-
-//  return overlayElement;
-// }
+burger.addEventListener('click' , toggleMenu);
