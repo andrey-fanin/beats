53bd8946b74d618f2a15e1005d8e6ada1e1c842e
Andrey Fanin <andr.fanin16@yandex.ru>
2021-04-27 19:34:57 +0500

    Удалил из проекта ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,69 +1,51 @@
-const myForm = document.querySelector('#myForm');
-const send = document.querySelector('#send');
+// const myForm = document.querySelector('#myForm');
+// const send = document.querySelector('#send');
 
-send.addEventListener('click', event => {
-    event.preventDefault();
+// send.addEventListener('click', event => {
+//     event.preventDefault();
 
-    if (validateForm(myForm)) {
-        const data = {
-            name: myForm.elements.name.value,
-            phone: myForm.elements.phone.value,
-            email: myForm.elements.email.value,
-            street: myForm.elements.street.value,
-            house: myForm.elements.house.value,
-            floor: myForm.elements.floor.value,
-            payment: myForm.elements.payment.value,
-            callback: myForm.elements.callback.checked
-        };
+//     if (validateForm(myForm)) {
+//         const data = {
+//             name: myForm.elements.name.value,
+//             phone: myForm.elements.phone.value,
+//             email: myForm.elements.email.value,
+//             comment: myForm.elements.comment.value,
+//         };
 
 
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = 'json';
-        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-        xhr.setRequestHeader('content-type', 'application/json');
-        xhr.send(JSON.stringify(data));
-        xhr.addEventListener('load', () => {
-           if (xhr.response.status) {                       // статус - то, чем отвечает сервер
-               console.log('ok');
-           }
-        });
-    }
-});
+//         const xhr = new XMLHttpRequest();
+//         xhr.responseType = 'json';
+//         xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+//         xhr.setRequestHeader('content-type', 'application/json');
+//         xhr.send(JSON.stringify(data));
+//         xhr.addEventListener('load', () => {
+//            if (xhr.response.status) {                    
+//                console.log('ok');
+//            }
+//         });
+//     }
+// });
 
-function validateForm(form){
-    let valid = true;
+// function validateForm(form){
+//     let valid = true;
 
-    if (!validateField(form.elements.name)) {
-        valid = false;
-    }
-    
-    if (!validateField(form.elements.phone)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.email)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.street)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.house)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.floor)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.payment)) {
-        valid = false;
-    }
-    if (!validateField(form.elements.callback)) {
-        valid = false;
-    }
+//     if (!validateField(form.elements.name)) {
+//         valid = false;
+//     };    
+//     if (!validateField(form.elements.phone)) {
+//         valid = false;
+//     };
+//     if (!validateField(form.elements.email)) {
+//         valid = false;
+//     };
+//     if (!validateField(form.elements.comment)) {
+//         valid = false;
+//     };
 
-    return valid;
+//     return valid;
+// }
 
-}
-
-function validateField(field) {
-    field.nextElementSibling.textContent = field.validationMessage;
-    return field.checkValidity();
-}
+// function validateField(field) {
+//     field.nextElementSibling.textContent = field.validationMessage;
+//     return field.checkValidity();
+// }
